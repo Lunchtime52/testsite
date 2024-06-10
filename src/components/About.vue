1d2d0dd24ea6bac2e6d4cb1ebb8cc27bc7e44f74
ryanbuchanan52.tn <ryanbuchanan52.tn@gmail.com>
2024-06-10 05:06:13 +0000

    bed time

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,15 +1,25 @@
 
 
 <template>
-    <div class="flex flex-col-reverse md:flex-row">
-        <div class="w-full md:w-1/2">
-            <h4>KNOXVILLE WEB DESIGN & DIGITAL MARKETING
-</h4>
-<h2>Everything you need to
-grow your business.</h2>
-        </div>
-        <div class="w-full md:w-1/2">
-            <img src="../assets/images/about-us-image.webp" alt="About Us Image">
-        </div>
-    </div>
+  <div id="hero"> <h2>Cracked Windshield? <br>We've Got You Covered!</h2>
+   <h4>Fast, Reliable Auto Glass Repair & Replacement</h4>
+   <button>Get a Free Quote Today!</button></div>
 </template>
+
+<style scoped>
+
+button{
+    border:3px solid rgb(5, 111, 14);
+    padding:15px;
+    font-size: large;
+}
+#hero{
+    background-image: url('../assets/images/heroimg.png');
+/* Suggested code may be subject to a license. Learn more: ~LicenseLog:2385168930. */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-color: transparent;
+    
+}
+</style>
