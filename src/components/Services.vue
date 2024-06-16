f169fa75f7484d0d30333c8268360141e8184469
ryanbuchanan52.tn <ryanbuchanan52.tn@gmail.com>
2024-06-16 03:56:14 +0000

    EOD

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,41 +1,65 @@
 <template>
-    <h2>Serivces</h2>
-    <div class="row">
-  <div class="column">
-    <div class="card"><h4>Windshield Repair</h4></div>
+  <h2>Serivces</h2>
+  <div class="row">
+    <div class="column">
+      <div class="card">
+        <h4>Windshield Repair</h4>
+      </div>
+    </div>
+    <div class="column">
+      <div class="card">
+        <h4>Windshield Replacement</h4>
+      </div>
+    </div>
+    <div class="column">
+      <div class="card">
+        <h4>Rock Chip Repair</h4>
+      </div>
+    </div>
   </div>
-  <div class="column">
-    <div class="card"><h4>Windshield Replacement</h4></div>
+
+  <div class="row">
+    <div class="column">
+      <div class="card">
+        <h4>Auto Glass Repair</h4>
+      </div>
+    </div>
+    <div class="column">
+      <div class="card">
+        <h4>Auto Glass Replacement</h4>
+      </div>
+    </div>
   </div>
-  <div class="column">
-    <div class="card"><h4>Rock Chip Repair</h4></div>
-  </div>
-  
-</div>
 </template>
 
 <style scoped>
-
 .column {
   float: left;
   width: 25%;
   padding: 0 10px;
   color: black;
+
 }
 
 /* Remove extra left and right margins, due to padding in columns */
-.row {margin: 0 -5px;}
+.row {
+  margin: 20px 5px;
+  justify-content: center;
+  display: flex;
+
+}
 
 /* Clear floats after the columns */
-.row:after {
+/* .row:after {
   content: "";
   display: table;
   clear: both;
-}
+} */
 
 /* Style the counter cards */
 .card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); /* this adds the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  /* this adds the "card" effect */
   padding: 16px;
   text-align: center;
   background-color: #f1f1f1;
