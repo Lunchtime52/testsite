4e49fcaaa127328bfa188ed125d196a5a81eb155
ryanbuchanan52.tn <ryanbuchanan52.tn@gmail.com>
2024-08-04 17:32:34 +0000

    Added TODO's

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,18 @@
 <script setup>
-import About from '../components/About.vue';
-import Services from '../components/Services.vue';
-
+import About from "../components/About.vue";
+import Services from "../components/Services.vue";
 </script>
 
 <template>
-    <About></About>
-    <Services></Services>
+  <About></About>
+  <Services></Services>
+  <!-- TODO 
+- add services page.
+- add Contact secation
+- add FAQ secation and or Page
+- add Testomonial section and or Page
+
+Remake Components to be resuable between pages and projects such as cards component, text with image, Test only, contact form, Hero image.
+insperation from Lumongwebdesign.com
+-->
 </template>
